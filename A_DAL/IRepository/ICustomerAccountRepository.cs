bcd56ff7dd9c6b4145a35a281316da2edbac2dc2
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-08 09:43:44 +0700

    add customerrepos

---
+++ A_DAL/IRepository/ICustomerAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using A_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_DAL.IRepository
+{
+    internal interface ICustomerAccountRepository
+    {
+        IEnumerable<CustomerAccount> GetAllCustomerAccount();
+        List<CustomerAccount> GetAllCustomerAccountByName(string name);
+        bool CreateCustomerAccount(CustomerAccount customerAccount);
+        bool UpdateCustomerAccount(CustomerAccount customerAccount);
+        bool DeleteCustomerAccount(string customerAccountID);
+    }
+}
