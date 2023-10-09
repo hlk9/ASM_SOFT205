aa58d9a33d69f7071c25983d19298786a098e48e
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-10 00:32:02 +0700

    add user model

--- B_BUS/Utilities/User.cs
+++ B_BUS/Utilities/User.cs
@@ -9,23 +9,26 @@ namespace B_BUS.Utilities
     public class User
     {
         private string userID;
-        private string role;
+        private byte role;
         private List<string> permissions;
+        private string name;
 
         public User()
         {
             
         }
 
-        public User(string userID, string role, List<string> permissions)
+        public User(string userID, byte role, List<string> permissions, string name)
         {
             this.userID = userID;
             this.role = role;
             this.permissions = permissions;
+            this.name = name;
         }
 
         public string UserID { get => userID; set => userID = value; }
-        public string Role { get => role; set => role = value; }
+        public byte Role { get => role; set => role = value; }
         public List<string> Permissions { get => permissions; set => permissions = value; }
+        public string Name { get => name; set => name = value; }
     }
 }
