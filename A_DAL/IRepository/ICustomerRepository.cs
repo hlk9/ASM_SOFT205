0177ef6dfdd18972b640106c36c9dfc1e97bd140
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-12 00:15:04 +0700

    add ICustomer

---
+++ A_DAL/IRepository/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using A_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_DAL.IRepository
+{
+    internal interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetAllCustomer();
+        List<Customer> GetAllCustomerByName(string name);
+        bool CreateCustomer(Customer customer);
+        bool UpdateCustomer(Customer customer);
+        bool DeleteCustomer(string customerID);
+    }
+}
