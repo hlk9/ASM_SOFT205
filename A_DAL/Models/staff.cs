244f0fc03573afd6685965667e1e795a9e4f7084
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-13 12:40:44 +0700

    add staff model

---
+++ A_DAL/Models/staff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_DAL.Models
+{
+    public partial class staff
+    {
+        public staff()
+        {
+            ProductImports = new HashSet<ProductImport>();
+            StaffAccounts = new HashSet<StaffAccount>();
+        }
+
+        public string StaffId { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+        public byte Sex { get; set; }
+        public string PhoneNumber { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+        public string? StaffAddress { get; set; }
+        public string? Email { get; set; }
+        public byte? StaffRole { get; set; }
+        public virtual ICollection<ProductImport> ProductImports { get; set; }
+        public virtual ICollection<StaffAccount> StaffAccounts { get; set; }
+    }
+}
