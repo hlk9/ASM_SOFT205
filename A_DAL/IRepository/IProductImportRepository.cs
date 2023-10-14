df6044d05d58e82d6e6cc8131eee460b41d0c257
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-15 00:18:27 +0700

    add product import interface

---
+++ A_DAL/IRepository/IProductImportRepository.cs
@@ -0,0 +1,20 @@
+﻿using A_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_DAL.IRepository
+{
+    internal interface IProductImportRepository
+    {
+        IEnumerable<ProductImport> GetAllProductImport();
+        List<ProductImport> GetAllProductImportByName();
+        bool CreatingProductImport(ProductImport productImport);
+        bool UpdateProductImport(ProductImport productImport);
+        bool DeleteProductImport(string importID);
+        ProductImport GetOne(string importID);
+
+    }
+}
