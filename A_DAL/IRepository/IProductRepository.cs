3af9f76704b76d34ecae7e5ece6b69dac71e8962
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-14 16:03:10 +0700

    add product interface

---
+++ A_DAL/IRepository/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using A_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_DAL.IRepository
+{
+    internal interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProduct();
+        List<Product> GetAllProductsByName(string name);
+        bool CreateProduct(Product product);
+        bool UpdateProduct(Product product);
+        bool DeleteProduct(string productID);
+    }
+}
