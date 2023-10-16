cab1b633226d51dda26f42ff520180c6611e72be
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-16 08:57:59 +0700

    add staff interface

---
+++ A_DAL/IRepository/IStaffRepository.cs
@@ -0,0 +1,19 @@
+﻿using A_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_DAL.IRepository
+{
+    internal interface IStaffRepository
+    {
+        IEnumerable<staff> GetAllStaff();
+        List<staff> GetStaffByName(string name);
+        bool CreateStaff(staff staff);
+        bool DeleteStaff(string staffId);
+        bool UpdateStaff(staff staff);
+        staff GetOne(string staffId);
+    }
+}
