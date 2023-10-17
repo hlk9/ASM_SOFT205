ba630cb25f7ff05b745178c2086445708987be09
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-17 09:35:14 +0700

    add staff account interface

---
+++ A_DAL/IRepository/IStaffAccountRepository.cs
@@ -0,0 +1,19 @@
+﻿using A_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_DAL.IRepository
+{
+    internal interface IStaffAccountRepository
+    {
+        IEnumerable<StaffAccount> GetAllStaffAccount();
+        List<StaffAccount> GetStaffAccountByName(string name);
+        bool CreateStaffAccount(StaffAccount staffAccount);
+        bool UpdateStaffAccount(StaffAccount staffAccount);
+        bool DeleteStaffAccount(string acountID);    
+
+    }
+}
