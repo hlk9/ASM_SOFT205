151ecea7d2bcd5936b14f4f973f207e7a58d0cc2
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-19 00:26:40 +0700

    add customer order model

---
+++ A_DAL/Models/CustomerOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_DAL.Models
+{
+    public partial class CustomerOrder
+    {
+        public CustomerOrder()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public string OrderId { get; set; } = null!;
+        public string CustomerId { get; set; } = null!;
+        public double OrderValue { get; set; }
+        public byte OrderStatus { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public virtual Customer Customer { get; set; } = null!;
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
