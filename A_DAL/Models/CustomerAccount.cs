e4b9d26fceee8560ff63815bf5e96dea0cd79c81
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-18 10:18:20 +0700

    add customer model

---
+++ A_DAL/Models/CustomerAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_DAL.Models
+{
+    public partial class CustomerAccount
+    {
+        public string AccountId { get; set; } = null!;
+        public string CustomerId { get; set; } = null!;
+        public string? UserName { get; set; }
+        public string? UPassword { get; set; }
+        public DateTime? LastLogin { get; set; }
+
+        public virtual Customer Customer { get; set; } = null!;
+    }
+}
