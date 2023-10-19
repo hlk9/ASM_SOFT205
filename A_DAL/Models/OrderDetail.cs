69012b28f4c86f5e0f59695bfe48edd66e89d398
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-20 00:05:02 +0700

    add order detail model

---
+++ A_DAL/Models/OrderDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_DAL.Models
+{
+    public partial class OrderDetail
+    {
+        public string OrderDetailId { get; set; } = null!;
+        public string OrderId { get; set; } = null!;
+        public string ProductId { get; set; } = null!;
+        public int Amount { get; set; }
+
+        public virtual CustomerOrder Order { get; set; } = null!;
+        public virtual Product Product { get; set; } = null!;
+    }
+}
