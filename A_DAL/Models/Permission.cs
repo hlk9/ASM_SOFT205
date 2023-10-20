6ce7721ad7957b0fc53f485dc3f535b25b2fd31d
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-21 00:10:18 +0700

    add permission model

---
+++ A_DAL/Models/Permission.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_DAL.Models
+{
+    public partial class Permission
+    {
+        public byte IdPermission { get; set; }
+        public string PermissionName { get; set; } = null!;
+    }
+}
