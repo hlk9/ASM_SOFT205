cb3656b7b2f2d2f366d40db88101bceb7185eb5b
Hoang Long <hoanglong.cmb@gmail.com>
2023-10-23 16:27:51 +0700

    add  product import model

---
+++ A_DAL/Models/ProductImport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_DAL.Models
+{
+    public partial class ProductImport
+    {
+        public int ImportId { get; set; }
+        public string? StaffId { get; set; }
+        public string ProductId { get; set; } = null!;
+        public DateTime? ImportDate { get; set; }
+        public int ImportQty { get; set; }
+        public decimal ImportPrice { get; set; }
+
+        public virtual Product Product { get; set; } = null!;
+        public virtual staff? Staff { get; set; }
+    }
+}
